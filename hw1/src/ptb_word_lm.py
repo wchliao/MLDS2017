d9df573a6ac6668d497d184033d18acc41650446
Frederic Go <fredericstgo@gmail.com>
2017-03-21 11:43:48 +0800

    nce

--- hw1/src/ptb_word_lm.py
+++ hw1/src/ptb_word_lm.py
@@ -150,11 +150,24 @@ class PTBModel(object):
     softmax_w = tf.get_variable(
         "softmax_w", [size, vocab_size], dtype=data_type())
     softmax_b = tf.get_variable("softmax_b", [vocab_size], dtype=data_type())
+    
     logits = tf.matmul(output, softmax_w) + softmax_b
-    loss = tf.contrib.legacy_seq2seq.sequence_loss_by_example(
-        [logits],
-        [tf.reshape(input_.targets, [-1])],
-        [tf.ones([batch_size * num_steps], dtype=data_type())])
+    
+    #loss = tf.contrib.legacy_seq2seq.sequence_loss_by_example(
+    #    [logits],
+    #    [tf.reshape(input_.targets, [-1])],
+    #    [tf.ones([batch_size * num_steps], dtype=data_type())])
+
+    num_samples = 10
+    labels = tf.reshape(input_.targets, [-1,1])
+    hidden = output
+    w_t = tf.transpose(softmax_w)
+    loss = tf.nn.sampled_softmax_loss(w_t, softmax_b,
+                                      labels,
+                                      hidden,  
+                                      num_samples,
+                                      vocab_size)
+
     self._cost = cost = tf.reduce_sum(loss) / batch_size
     self._final_state = state
 
@@ -210,12 +223,12 @@ class SmallConfig(object):
   num_layers = 1
   num_steps = 20
   hidden_size = 200
-  max_epoch = 4
+  max_epoch = 2
   max_max_epoch = 13
   keep_prob = 1.0
   lr_decay = 0.5
   batch_size = 20
-  vocab_size = 12001
+  vocab_size = 12000
 
 
 class MediumConfig(object):
@@ -263,7 +276,7 @@ class TestConfig(object):
   keep_prob = 1.0
   lr_decay = 0.5
   batch_size = 20
-  vocab_size = 10000
+  vocab_size = 12000
 
 
 def run_epoch(session, model, eval_op=None, verbose=False):
@@ -312,6 +325,39 @@ def get_config():
     return TestConfig()
   else:
     raise ValueError("Invalid model: %s", FLAGS.model)
+
+
+def predict(session, model):
+  """Runs the model on the given data."""
+  start_time = time.time()
+
+  state = session.run(model.initial_state)
+
+  fetches = {
+      "logits": model.logits
+  }
+  if eval_op is not None:
+    fetches["eval_op"] = eval_op
+
+  for step in range(model.input.epoch_size):
+    feed_dict = {}
+    for i, (c, h) in enumerate(model.initial_state):
+      feed_dict[c] = state[i].c
+      feed_dict[h] = state[i].h
+
+    vals = session.run(fetches, feed_dict)
+    cost = vals["cost"]
+    state = vals["final_state"]
+
+    costs += cost
+    iters += model.input.num_steps
+
+    if verbose and step % (model.input.epoch_size // 10) == 10:
+      print("%.3f perplexity: %.3f speed: %.0f wps" %
+            (step * 1.0 / model.input.epoch_size, np.exp(costs / iters),
+             iters * model.input.batch_size / (time.time() - start_time)))
+
+  return np.exp(costs / iters)
 
 
 def main(_):
