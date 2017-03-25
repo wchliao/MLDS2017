d75d4e7856b5d3926c5d93b5bee3e0d268882edc
Frederic Go <fredericstgo@gmail.com>
2017-03-25 22:38:43 +0800

    predict

--- hw1/src/ptb_word_lm.py
+++ hw1/src/ptb_word_lm.py
@@ -1,52 +1,4 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Example / benchmark for building a PTB LSTM model.
-
-Trains the model described in:
-(Zaremba, et. al.) Recurrent Neural Network Regularization
-http://arxiv.org/abs/1409.2329
-
-There are 3 supported model configurations:
-===========================================
-| config | epochs | train | valid  | test
-===========================================
-| small  | 13     | 37.99 | 121.39 | 115.91
-| medium | 39     | 48.45 |  86.16 |  82.07
-| large  | 55     | 37.87 |  82.62 |  78.29
-The exact results may vary depending on the random initialization.
-
-The hyperparameters used in the model:
-- init_scale - the initial scale of the weights
-- learning_rate - the initial value of the learning rate
-- max_grad_norm - the maximum permissible norm of the gradient
-- num_layers - the number of LSTM layers
-- num_steps - the number of unrolled steps of LSTM
-- hidden_size - the number of LSTM units
-- max_epoch - the number of epochs trained with the initial learning rate
-- max_max_epoch - the total number of epochs for training
-- keep_prob - the probability of keeping weights in the dropout layer
-- lr_decay - the decay of the learning rate for each epoch after "max_epoch"
-- batch_size - the batch size
-
-The data required for this example is in the data/ dir of the
-PTB dataset from Tomas Mikolov's webpage:
-
-$ wget http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz
-$ tar xvf simple-examples.tgz
-
+"""
 To run:
 
 $ python ptb_word_lm.py --data_path=simple-examples/data/
@@ -82,7 +34,6 @@ FLAGS = flags.FLAGS
 def data_type():
   return tf.float16 if FLAGS.use_fp16 else tf.float32
 
-
 class PTBInput(object):
   """The input data."""
 
@@ -104,14 +55,18 @@ class PTBModel(object):
   def __init__(self, is_training, config, input_):
     self._input = input_
 
-    batch_size = input_.batch_size
-    num_steps = input_.num_steps
+    if is_training:
+      batch_size = input_.batch_size
+      num_steps = input_.num_steps
+    else:
+      batch_size = config.batch_size
+      num_steps = config.num_steps
+
     size = config.hidden_size
     vocab_size = config.vocab_size
 
-    # Slightly better results can be obtained with forget gate biases
-    # initialized to 1 but the hyperparameters of the model would need to be
-    # different than reported in the paper.
+    self.question = tf.placeholder(tf.int32, [1, None])
+
     def lstm_cell():
       return tf.contrib.rnn.BasicLSTMCell(
           size, forget_bias=0.0, state_is_tuple=True)
@@ -128,20 +83,14 @@ class PTBModel(object):
     with tf.device("/cpu:0"):
       embedding = tf.get_variable(
           "embedding", [vocab_size, size], dtype=data_type())
-      inputs = tf.nn.embedding_lookup(embedding, input_.input_data)
+      if is_training:
+        inputs = tf.nn.embedding_lookup(embedding, input_.input_data)
+      else:
+        inputs = tf.nn.embedding_lookup(embedding, self.question)
 
     if is_training and config.keep_prob < 1:
       inputs = tf.nn.dropout(inputs, config.keep_prob)
 
-    # Simplified version of models/tutorials/rnn/rnn.py's rnn().
-    # This builds an unrolled LSTM for tutorial purposes only.
-    # In general, use the rnn() or state_saving_rnn() from rnn.py.
-    #
-    # The alternative version of the code below is:
-    #
-    # inputs = tf.unstack(inputs, num=num_steps, axis=1)
-    # outputs, state = tf.nn.rnn(cell, inputs,
-    #                            initial_state=self._initial_state)
     outputs = []
     state = self._initial_state
     with tf.variable_scope("RNN"):
@@ -151,16 +100,22 @@ class PTBModel(object):
         outputs.append(cell_output)
 
     output = tf.reshape(tf.concat(axis=1, values=outputs), [-1, size])
+
     softmax_w = tf.get_variable(
         "softmax_w", [size, vocab_size], dtype=data_type())
     softmax_b = tf.get_variable("softmax_b", [vocab_size], dtype=data_type())
     
-    logits = tf.matmul(output, softmax_w) + softmax_b
-    
+    self._logits = tf.nn.softmax(tf.matmul(output, softmax_w) + softmax_b)
+
     #loss = tf.contrib.legacy_seq2seq.sequence_loss_by_example(
     #    [logits],
     #    [tf.reshape(input_.targets, [-1])],
     #    [tf.ones([batch_size * num_steps], dtype=data_type())])
+    self._final_state = state
+
+
+    if not is_training:
+      return
 
     num_samples = 10
     labels = tf.reshape(input_.targets, [-1,1])
@@ -173,10 +128,6 @@ class PTBModel(object):
                                       vocab_size)
 
     self._cost = cost = tf.reduce_sum(loss) / batch_size
-    self._final_state = state
-
-    if not is_training:
-      return
 
     self._lr = tf.Variable(0.0, trainable=False)
     tvars = tf.trainable_variables()
@@ -205,6 +156,10 @@ class PTBModel(object):
   @property
   def cost(self):
     return self._cost
+
+  @property
+  def logits(self):
+    return self._logits
 
   @property
   def final_state(self):
@@ -273,13 +228,13 @@ class TestConfig(object):
   learning_rate = 1.0
   max_grad_norm = 1
   num_layers = 1
-  num_steps = 2
+  num_steps = 30
   hidden_size = 200
   max_epoch = 1
   max_max_epoch = 0
   keep_prob = 1.0
   lr_decay = 0.5
-  batch_size = 20
+  batch_size = 1
   vocab_size = 12000
 
 
@@ -317,6 +272,42 @@ def run_epoch(session, model, eval_op=None, verbose=False):
 
   return np.exp(costs / iters)
 
+def predict(session, model, questions):
+  """Runs the model on the given data."""
+  for q in questions:
+    left = q.left
+    if len(left) < 30:
+      for i in range(30-len(left)):
+        left += [0]
+
+    start_time = time.time()
+    costs = 0.0
+    iters = 0
+    state = session.run(model.initial_state)
+
+    fetches = {
+        "logits": model.logits,
+        "final_state": model.final_state,
+    }
+
+    feed_dict = {}
+    for i, (c, h) in enumerate(model.initial_state):
+      feed_dict[c] = state[i].c
+      feed_dict[h] = state[i].h
+
+    feed_dict[model.question] = np.array(left).reshape((1,-1))
+
+    vals = session.run(fetches, feed_dict)
+   
+    p_opt = []
+    for o in q.options:
+      p_opt.append(np.log(vals["logits"][q.pos-1, o]))
+    print(p_opt)
+    #logits = session.run(model.logits)
+    #print(logits[0])
+
+  return None
+
 
 def get_config():
   if FLAGS.model == "small":
@@ -339,7 +330,7 @@ def main(_):
   config = get_config()
   eval_config = get_config()
   eval_config.batch_size = 1
-  eval_config.num_steps = 1
+  eval_config.num_steps = 30
 
   with tf.Graph().as_default():
     initializer = tf.random_uniform_initializer(-config.init_scale,
@@ -356,7 +347,7 @@ def main(_):
       test_input = PTBInput(config=eval_config, data=train_data, name="TestInput")
       with tf.variable_scope("Model", reuse=True, initializer=initializer):
         mtest = PTBModel(is_training=False, config=eval_config,
-                         input_=test_input)
+                         input_=test_questions)
 
     sv = tf.train.Supervisor(logdir=FLAGS.save_path)
     session_config = tf.ConfigProto()
@@ -365,13 +356,11 @@ def main(_):
       for i in range(config.max_max_epoch):
         lr_decay = config.lr_decay ** max(i + 1 - config.max_epoch, 0.0)
         m.assign_lr(session, config.learning_rate * lr_decay)
-
         print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
         train_perplexity = run_epoch(session, m, eval_op=m.train_op,
                                      verbose=True)
 
-      test_perplexity = run_epoch(session, mtest)
-      print("Test Perplexity: %.3f" % test_perplexity)
+      test_perplexity = predict(session, mtest, test_questions)
 
       if FLAGS.save_path:
         print("Saving model to %s." % FLAGS.save_path)
