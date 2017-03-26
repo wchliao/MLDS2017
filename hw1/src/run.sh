2b15c0e173d4d4a1551aed6b94219013973240cc
Frederic Go <fredericstgo@gmail.com>
2017-03-26 22:43:11 +0800

    run.sh

--- hw1/src/run.sh
+++ hw1/src/run.sh
@@ -4,6 +4,6 @@ if (( $# != 2 )); then
     exit
 fi
 
-#wget https://dl.dropboxusercontent.com/u/16850414/model.tar.gz
-#tar xzvf model.tar.gz
-python ptb_word_lm_final.py --q_path=$1 --save_path=../../../model_old --p_path=$2
+wget https://dl.dropboxusercontent.com/u/16850414/model.tar.gz
+tar xzvf model.tar.gz
+python ptb_word_lm_final.py --q_path=$1 --save_path=./model_old --p_path=$2
