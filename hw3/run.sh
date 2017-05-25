0d03d0873549f348a19abc43aba237bcf4e41ae4
s101062161 <ahpong129@hotmail.com>
2017-05-25 19:04:46 +0800

    edited path in run.sh

--- hw3/run.sh
+++ hw3/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python download_files.py
-python generate_thought_vectors.py --caption_file=$1
-python generate_images.py
+python src/download_files.py
+python src/generate_thought_vectors.py --caption_file=$1
+python src/generate_images.py
