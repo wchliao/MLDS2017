6681b49a62e45ae4adfc477e2e0a82179726b3ca
s101062161 <ahpong129@hotmail.com>
2017-06-15 16:28:36 +0800

    fixed string compare in run.sh

--- hw4/run.sh
+++ hw4/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # bash run.sh S2S input.txt prediction.txt
 
-if [ $1 -eq "S2S" ]
+if [ $1 = "S2S" ]
 then
 	echo "> RUNNING S2S"
 	python3 src/download_models_s2s.py
 	python3 src/main.py --mode test --model_name open_subtitles --vocab_size 80000 --size 128 --antilm 0.7 --n_bonus 0.1 --rev_model 0 --reinforce_learn 0 --input_name $2 --output_name $3
-elif [ $1 -eq "RL" ]
+elif [ $1 = "RL" ]
 then
 	echo "> RUNNING RL"
 	python3 src/download_models_rl.py
 	python3 src/main.py --mode test --model_name open_subtitles_best --vocab_size 80000 --size 128 --antilm 0.7 --n_bonus 0.1 --rev_model 1 --reinforce_learn 1 --input_name $2 --output_name $3
-elif [ $1 -eq "BEST" ]
+elif [ $1 = "BEST" ]
 then
 	echo "> RUNNING BEST"
 	python3 src/download_models_s2s.py
