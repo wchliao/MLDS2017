52aaa8159b56f4ac6c85cbf8794f422c2db2fd36
s101062161 <ahpong129@hotmail.com>
2017-06-15 16:37:22 +0800

    changed import path in main.py

--- hw4/src/main.py
+++ hw4/src/main.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import tensorflow as tf
 
-import src/lib
+import src.lib
 
 from lib.config import params_setup
 from lib.train import train
